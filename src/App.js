8994abe64149c019cd29fae478a2fd4afa3c7423
ZH Zhang <zhangzhhz@gmail.com>
2020-01-09 17:43:51 +0000

    Setting up our React app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import Routes from './Routes';
 import './App.css';
 
@@ -16,8 +17,12 @@ function App() {
         </Navbar.Header>
         <Navbar.Collapse>
           <Nav pullRight>
-            <NavItem href="/signup">Signup</NavItem>
-            <NavItem href="/login">Login</NavItem>
+            <LinkContainer to="/signup">
+              <NavItem>Signup</NavItem>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-northeast-1",
+    BUCKET: "notes-app-uploads-zhce19"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://fmqb8ngtv1.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-northeast-1",
+    USER_POOL_ID: "ap-northeast-1_Xu0VwRjZR",
+    APP_CLIENT_ID: "2ocmci1hlnoje5sv777mnrfb4f",
+    IDENTITY_POOL_ID: "ap-northeast-1:0eef3a29-197e-46a5-802f-d3fe1f2d4282"
+  }
+};
